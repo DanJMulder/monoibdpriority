c25b75ce83570b1cb4bc5aa3d8c683d4001e2592
Michael Li <michael.li@sickkids.ca>
2021-03-09 11:12:42 -0500

    General cleanup. Added new parameters to function for future work.

--- Part_1_fam_member_vcf.R
+++ Part_1_fam_member_vcf.R
@@ -4,53 +4,51 @@
 
 
 # To use this script, the following user changes are needed:
-  # 1. change "/path/to/files" on the line below to the local path to the folder containing only the family member VCFs (not proband VCFs)
-      path_to_files <- "path/to/files"
-  # 2. change the "number_of_header_rows" variable on the line below to how many header rows in your VCFs (that need to be skipped)
-      skip <- number_of_header_rows
+# 1. change "/path/to/files" on the line below to the local path to the folder containing only the family member VCFs (not proband VCFs)
+input_vcf_dir <- "path/to/files"
+# 2. change the "number_of_header_rows" variable on the line below to how many header rows in your VCFs (that need to be skipped)
+skip <- number_of_header_rows
 
 
 library(tidyverse)
 library(glue) #for interpreting string literals
 
+files <- list.files(glue("{input_vcf_dir}"))
 
-files <- list.files(glue("{path_to_files}"))
-
-
-fam_memb_processing <- function(input) {
+fam_memb_processing <- function(input, output, num_header) {
   #load in vcf
-  vcf <- read_delim(input, 
-                    "\t", escape_double = FALSE, trim_ws = TRUE, 
-                    skip = skip, col_types = cols('#CHROM' = col_factor()))
+  vcf <- read_delim(input,
+                    "\t", escape_double = FALSE, trim_ws = TRUE,
+                    skip = num_header, col_types = cols('#CHROM' = col_factor()))
   names(vcf)[names(vcf) == "#CHROM"] <- "chr"
   vcf$chr <- as.factor(gsub('chr', '', vcf$chr))
-  
+
   #creating address column to enable merge with proband vcf
   vcf$address <- paste0(vcf$chr, ":", vcf$POS)
   vcf$address <- paste0(vcf$address, ":", vcf$REF)
   vcf$address <- paste0(vcf$address, ":", vcf$ALT)
-  
+
   #create het_or_hom from GENOTYPE column########################
   names(vcf)[10] <- "het_or_hom"
   het_or_hom <- vcf$het_or_hom
   vcf$het_or_hom <- substring(het_or_hom, 1, 3)
   vcf$het_or_hom <- as.factor(vcf$het_or_hom)
-  
+
   vcf$het_or_hom <- gsub('0/1', 'het', vcf$het_or_hom)
   vcf$het_or_hom <- gsub('1/1', 'hom_alt', vcf$het_or_hom)
   vcf$het_or_hom <- gsub('1/2', 'hom_alt', vcf$het_or_hom)
   vcf$het_or_hom <- as.factor(vcf$het_or_hom)
-  
+
   rm(het_or_hom)
-  
+
   vcf <- subset(vcf, select = c(address, het_or_hom))
-  
-  write_csv(vcf, input)
-  
+
+  write_csv(vcf, output)
+
   rm(vcf)
 }
-  
+
 #loop through input files
-for (i in 1:length(files)) {
-  fam_memb_processing(files[i])
+for (i in seq_along(files)) {
+  fam_memb_processing(files[i], files[i], skip)
 }
